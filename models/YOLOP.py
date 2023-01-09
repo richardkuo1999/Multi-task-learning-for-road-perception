8588ec909d4f3db3554536a803cf09900dc55d9b
KuoLiChia_windows <richardkuo1999@gmail.com>
2023-01-09 12:44:50 +0800

    YOLOP.py can run

--- models/YOLOP.py
+++ models/YOLOP.py
@@ -1,7 +1,17 @@
+import os
+import sys
 import yaml
 import torch
 import torch.nn as nn
 import math
+from pathlib import Path
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
 from utils.metrics import  SegmentationMetric
 from utils.autoanchor import check_anchor_order
 from utils.torch_utils import time_synchronized, initialize_weights
@@ -63,7 +73,7 @@ class MCnet(nn.Module):
         LL_fmap = []
         # print(x.size())
         for i, block in enumerate(self.model):
-            # print(i, block)
+            print(i, block)
             if block.from_ != -1:
                 x = cache[block.from_] if isinstance(block.from_, int) else [x if j == -1 else cache[j] for j in block.from_]       #calculate concat detect
             x = block(x)
