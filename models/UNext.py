4d323a52117a469ed4c54f969dc908110fc4f015
KuoLiChia_windows <richardkuo1999@gmail.com>
2023-02-28 23:41:05 +0800

    let Unext can run but dirty code

--- models/UNext.py
+++ models/UNext.py
@@ -17,6 +17,7 @@ import torch.nn.functional as F
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 # import types
 import math
+# from collections import OrderedDict
 # from abc import ABCMeta, abstractmethod
 # import pdb
 
