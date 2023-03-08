7067215657112f43905de6b731eacd2447c75d4b
KuoLiChia_windows <richardkuo1999@gmail.com>
2023-03-08 19:42:30 +0800

    add LayerNorm at every MLP

--- models/common.py
+++ models/common.py
@@ -141,11 +141,12 @@ class MLP(nn.Module):
     def __init__(self, input_dim=2048, embed_dim=768):
         super().__init__()
         self.proj = nn.Linear(input_dim, embed_dim)
+        self.ln = nn.LayerNorm(embed_dim)
 
     def forward(self, x):
         x = x.flatten(2).transpose(1, 2)
         x = self.proj(x)
-        return x
+        return self.ln(x)
     
 class OverlapPatchEmbed(nn.Module):
     """ Image to Patch Embedding
